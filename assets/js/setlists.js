02ca0adf793c46b28e76bebbf25e67413d1fa16b
Kieran Sweeden <kieransweeden@gmail.com>
2021-10-02 16:20:44 +0000

    add push to json to local storage functionality to setlists.js

--- assets/js/setlists.js
+++ assets/js/setlists.js
@@ -1,19 +1,20 @@
 // When DOM content is loaded...
 window.addEventListener('DOMContentLoaded', () => {
-    // ...determine what content the page will be dealing with
+    // Determine what content the page will be dealing with and store the type within a variable
     let contentType = determineContentType();
 
-    console.log(contentType);
+    // Knowing the type of content it'll be dealing with, start the application with the contentType as a parameter
+    startGigMate(contentType);
 })
 
-function determineContentType(){
-    // To determine what content type this page will be dealing with, grab the current file name
+function determineContentType() {
+    // To determine what content type this page will be dealing with, grab the current pathname
     let currentPage = window.document.location.pathname;
 
     // Initialise an empty variable to contain a content type value
     let contentType;
 
-    // Assign content type depending on value within current page
+    // Assign content type depending on the value within current page variable
     if (currentPage === "/setlists.html") {
         contentType = "setlists";
     } else if (currentPage === "/repertoire.html") {
@@ -24,4 +25,64 @@ function determineContentType(){
 
     // Return the content type variable
     return contentType;
+}
+
+function startGigMate(contentType) {
+    // Firstly, start local storage functionality to determine what data items GigMate will be working with
+    startLocalStorage(contentType);
+}
+
+function startLocalStorage(contentType) {
+    // Check if there is already data present within local storage
+    let hasLocalStorage = checkLocalStorage(contentType);
+
+    // If there is...
+    if (hasLocalStorage === true) {
+        // ...local storage, retrieve the data from local storage
+        retrieveLocalStorageData(contentType);
+    } else if (hasLocalStorage === false) {
+        // ...no local storage, start GigMate with the appropriate initialised json file
+        addInitialisedJSONToLocalStorage(contentType);
+    }
+}
+
+function checkLocalStorage(contentType) {
+    // Return true if local storage exists, return false if not
+    return (localStorage.hasOwnProperty(contentType)) ? true : false;
+}
+
+function retrieveLocalStorageData(contentType) {
+
+}
+
+async function addInitialisedJSONToLocalStorage(contentType) {
+    // Initialise a variable to store the local JSON file data
+    let localJSONData;
+
+    // If the contentType is...
+    if (contentType === "setlists") {
+        // ...setlists, fetch & store the setlist JSON data
+        localJSONData = await getJSONData("assets/json/initSetlists.json");
+    } else if (contentType === "repertoire") {
+        // ...repertoire, fetch & store the repertoire JSON data
+        localJSONData = await getJSONData("assets/json/initRepertoire.json");
+    } else if (contentType === "gigs") {
+        // ...gigs, fetch & store the gigs JSON data
+        localJSONData = await getJSONData("assets/json/initGigs.json");
+    }
+
+    // Push the local JSON data to local storage
+    pushToLocalStorage(contentType, localJSONData);
+}
+
+async function getJSONData(pathToJSONFile) {
+    // Fetch & store the data contained within the JSON file
+    let fileData = await fetch(pathToJSONFile);
+    // Collect the json within the fileData promise and return it
+    return fileData.json();
+}
+
+function pushToLocalStorage(contentType, localJSONData) {
+    // Push the contentType(key) & localJSONData(value) to local storage
+    localStorage.setItem(contentType, JSON.stringify(localJSONData));
 }
